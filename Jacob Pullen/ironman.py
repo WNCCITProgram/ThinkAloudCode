96e693402bc15604fc46a4223bd4fe0896264152
Jacob <pullenj1@wncc.edu>
2022-04-16 17:39:35 -0500

    Create ironman.py
    3 lines of code to draw ironman

---
+++ Jacob Pullen/ironman.py
@@ -0,0 +1,15 @@
+"""
+    Name:ironman.py
+    Author:Jacob Pullen
+    Created:04/16/22
+    Purpose: Draw a picture of ironman in 3 lines of code
+"""
+
+#import the sketchpy library
+from sketchpy import library as lib
+
+#Assign the desired picture to a variable
+obj = lib.rdj()
+
+#Command to draw the desired picture
+obj.draw()
